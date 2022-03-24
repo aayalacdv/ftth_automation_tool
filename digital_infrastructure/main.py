3d910a46e63227c56bbbd95eb914c90ca9dc1ce6
AXEL AYALA CORDOVA <AAYALA@sirius.local>
2022-03-24 10:10:04 +0100

    added better keyboard support

--- digital_infrastructure/main.py
+++ digital_infrastructure/main.py
@@ -19,40 +19,32 @@ if __name__ == '__main__':
     cable_manager = CableManager(driver=driver, working_cluster=WORKING_CLUSTER, working_town_code='CHT', working_town=WORKING_TOWN) 
 
     keep_open = True
-    
 
-
+    print("Eneter a command\n")
     while keep_open:
-        # command = input("ENTER A COMMAND \n") 
-        command = 'ñaslkdfjalskd'
-        if(command == 'o'): 
-            keep_open = False
-        elif( command == 'º'): 
+        if(keyboard.is_pressed('º+space')): 
             creator.create_joint_box(JointBoxCodes.JOINT_BOX_SMALL)
-        elif( command == 'ª'): 
+        if(keyboard.is_pressed('ª+space')): 
             creator.create_joint_box(JointBoxCodes.JOINT_BOX_MEDIAN)
-        elif( command == 'q'): 
+        if(keyboard.is_pressed('q+space') | keyboard.is_pressed('Q+space')): 
             creator.create_joint_chamber_sb(SbCodes.JOINT_CHAMBER_SB_12_CLIENTS)
-        elif(command == 'w'):
+        if(keyboard.is_pressed('w+space') | keyboard.is_pressed('W+space')): 
             creator.create_joint_chamber_sb(SbCodes.JOINT_CHAMBER_SB_MOS)
-        elif(command == 'e'):
+        if(keyboard.is_pressed('e+space') | keyboard.is_pressed('E+space')): 
             creator.create_joint_chamber_sb(SbCodes.JOINT_CHAMBER_SB_SAT)
-        elif(command == '1'):
-            creator.create_pole_sb(SbCodes.POLE_SB_24_CLIENTS)
-        elif(command == '2'):
-            creator.create_pole_sb(SbCodes.POLE_SB_24_CLIENTS_MOS)
-        elif(command == '3'):
-            creator.create_pole_sb(SbCodes.POLE_SB_24_CLIENTS_SAT)
-        elif(command == '0'): 
-            pass
-        
-        if keyboard.is_pressed('a+space'):
+        if(keyboard.is_pressed('1+space')): 
+           creator.create_pole_sb(SbCodes.POLE_SB_24_CLIENTS) 
+        if(keyboard.is_pressed('2+space')): 
+           creator.create_pole_sb(SbCodes.POLE_SB_24_CLIENTS_MOS) 
+        if(keyboard.is_pressed('3+space')): 
+           creator.create_pole_sb(SbCodes.POLE_SB_24_CLIENTS_SAT) 
+        if keyboard.is_pressed('a+space') | keyboard.is_pressed('A+space'):
             cable_manager.automate_cable_form(cable_template=CableCodes.CABLE_12_FO)
-        if keyboard.is_pressed('s+space'):
+        if keyboard.is_pressed('s+space') | keyboard.is_pressed('S+space'):
             cable_manager.automate_cable_form(cable_template=CableCodes.CABLE_48_FO)
-        if keyboard.is_pressed('d+space'):
+        if keyboard.is_pressed('d+space') | keyboard.is_pressed('D+space'):
             cable_manager.automate_cable_form(cable_template=CableCodes.CABLE_12_FO_ULW)
-        if keyboard.is_pressed('f+space'):
+        if keyboard.is_pressed('f+space') | keyboard.is_pressed('F+space'):
             cable_manager.automate_cable_form(cable_template=CableCodes.CABLE_48_FO_ULW)
 
 
